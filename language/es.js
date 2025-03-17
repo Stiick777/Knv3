06ed1d3a8bd4c5259f1d5aacad7e696bbc57dd11
Stiick777 <kanixxxcexx@gmail.com>
2025-03-17 13:50:02 -0500

    Add files via upload

--- language/es.js
+++ language/es.js
@@ -265,7 +265,7 @@ const translations = {
     adminH: 'Este comando es solo para *Admins* del grupos',
     botAdmin: '¡Para usar este comando debo ser *Administrador!*',
     unregH: 'Regístrese para usar esta función  Escribiendo:\n\n*/reg*',
-    ig: `▢ Sígueme en Instagram\n${fgig}`
+    ig: `▢ KanBot\nby Stiiven`
 }
 
 export default translations

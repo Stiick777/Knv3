58b605956b2cea94fee6ce32694f0242c9af1382
Stiick777 <kanixxxcexx@gmail.com>
2025-06-18 18:09:10 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,10 +4,10 @@ import fetch from 'node-fetch'
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
   const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}  
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/whpcLpM7/Screenshot-20250421-195814-2.png')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
-  let txt = 'ゲ◜៹ New Member ៹◞ゲ'
+  let txt = 'ゲ◜៹ Hola Member ៹◞ゲ'
   let txt1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
   let groupSize = participants.length
   if (m.messageStubType == 27) {
@@ -17,11 +17,20 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
+    let bienvenida = `╭══•🔥ೋ•๑♡๑•ೋ🔥•══╮\n` +
+          `¡Bienvenido/a, ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n╰══•🔥ೋ•๑♡๑•ೋ🔥•══╯\n`
+          `\n` + // Espacio adicional aquí
+          `Esperamos que disfrutes tu estancia en el grupo.\n` +
+          `*_Recuerda leer la descripción_*\n` +
+          `🥀*ੈ✩‧₊˚༺☆༻*ੈ✩˚🍁`    
     await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
   }
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+    let bye = `╭══•🔥ೋ•๑♡๑•ೋ🔥•══╮\n` +
+          `¡Adiós, ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n`+ `╰══•🔥ೋ•๑♡๑•ೋ🔥•══╯\n` +
+          `\n` + // Espacio adicional aquí
+          `Gracias por haber estado con nosotros.\n` +
+          `🥀*ੈ✩‧₊˚༺☆༻*ੈ✩˚🍁`;
     await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak)
   }}

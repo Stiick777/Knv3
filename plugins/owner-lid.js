73d25274fadeb1419ca0f42e9d330ddd88a8196f
Stiick777 <kanixxxcexx@gmail.com>
2025-06-18 18:33:56 -0500

    Update owner-lid.js

--- plugins/owner-lid.js
+++ plugins/owner-lid.js
@@ -1,6 +1,15 @@
+
 let handler = async (m, { conn }) => {
-const userLid = m.sender
-await conn.sendMessage(m.chat, { text: `Tu LID es: ${userLid}` }, { quoted: m })
+  // Obtener el ID del usuario target:
+  let userLid = m.sender // valor por defecto
+
+  if (m.quoted) {
+    userLid = m.quoted.sender // si se responde a alguien
+  } else if (m.mentionedJid && m.mentionedJid.length > 0) {
+    userLid = m.mentionedJid[0] // si se etiqueta a alguien
+  }
+
+  await conn.sendMessage(m.chat, { text: `El LID es: ${userLid}` }, { quoted: m })
 }
 
 handler.help = ['mylid', 'lid']

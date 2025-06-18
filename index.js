7c61bc490725f1571ed3dde320504db87d9c0bd7
Stiick777 <kanixxxcexx@gmail.com>
2025-06-18 18:19:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import fs, {readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileSy
 import yargs from 'yargs';
 import {spawn} from 'child_process'
 import lodash from 'lodash'
-import { yukiJadiBot } from './plugins/jadibot-serbot.js'
+import { yukiJadiBot } from './plugins/_yuta.js'
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import {tmpdir} from 'os'

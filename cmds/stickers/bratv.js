11b295c2348c694886f034f93657cd9037c432b3
Stiick777 <kanixxxcexx@gmail.com>
2026-07-06 22:30:57 -0500

    Update bratv.js

--- cmds/stickers/bratv.js
+++ cmds/stickers/bratv.js
@@ -23,7 +23,7 @@ export default {
       const name = user.name || msg.sender.split('@')[0];
       const meta1 = user.metadatos ? String(user.metadatos).trim() : '';
       const meta2 = user.metadatos2 ? String(user.metadatos2).trim() : '';
-      let texto1 = meta1 ? meta1 : 'ʏᴜᴋɪ 🧠 Wᴀʙᴏᴛ';
+      let texto1 = meta1 ? meta1 : '1✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰';
       let texto2 = meta1 ? (meta2 ? meta2 : '') : `@${name}`;
       const videoBuffer = await fetchStickerVideo(text);
       const tmpFile = `./tmp/bratv-${Date.now()}.mp4`;
